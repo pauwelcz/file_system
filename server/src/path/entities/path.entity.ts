0ab3d4dc9c76e06b08df7fb1b82e2c3fbc0993ff
pauwelcz <pavel.sedlar1993@gmail.com>
2024-10-12 14:42:45 +0200

    fix: delete unnecessary entity

--- server/src/path/entities/path.entity.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-
-@ObjectType()
-export class Path {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
-}
